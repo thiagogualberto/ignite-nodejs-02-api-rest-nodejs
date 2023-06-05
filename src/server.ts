bf82624ae523ef0e19fc761da3b718f8c0ab7a52
Thiago Gualberto <thiago.gou@gmail.com>
2023-06-05 16:15:23 -0300

    Banco de dados - Tratando env com Zod.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -22,7 +23,7 @@ app.post('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Runing!')
