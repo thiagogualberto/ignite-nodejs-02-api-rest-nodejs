6247a63a9bda7758a06afc6d18f72402363c083e
Thiago Gualberto <thiago.gou@gmail.com>
2023-06-12 10:34:22 -0300

    Implementando rotas - Plugins do Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,11 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
-})
-
-app.post('/hello', async () => {
-  const transaction = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Transação de teste',
-    amount: 1000,
-  })
-  .returning('*')
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
