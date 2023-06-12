af63e929bc2bdc0e3210afd72071d9e8ca4c2ed5
Thiago Gualberto <thiago.gou@gmail.com>
2023-06-12 19:02:19 -0300

    Implementando rotas - Utilizando cookies no Fastify

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Requisitos Funcionais(RF)
 [x] O usuário deve poder criar uma conta;
-[] O usuário deve poder obter um extrato da sua conta;
+[x] O usuário deve poder obter um extrato da sua conta;
 [x] O usuário deve poder listar todas transações que já ocorreram;
 [x] O usuário deve poder visualizar uma transação única;
 
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,7 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  //Adiciona transações
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -16,15 +17,26 @@ export async function transactionsRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+    if (!sessionId){
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
   })
 
+  //Lista todas as transações
   app.get('/', async () => {
     const transactions = await knex('transactions').select()
 
@@ -33,6 +45,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
     }
   })
 
+  //Lista uma única transação
   app.get('/:id', async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
@@ -49,6 +62,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
     }
   })
 
+  //Mostra o resumo das transações
   app.get('/summary', async () => {
     const summary = await knex('transactions').sum('amount', { as: 'amount' }).first()
 
