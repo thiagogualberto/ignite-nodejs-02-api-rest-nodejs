ed6963d823fab3ac295741422b2aa382ebeb27c1
Thiago Gualberto <thiago.gou@gmail.com>
2023-06-13 16:48:22 -0300

    Testes automatizados - Testando listagem de transações

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, test, describe } from 'vitest'
+import { afterAll, beforeAll, it, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -11,7 +11,7 @@ describe('Transactions routes', () => {
     await app.close()
   })
   
-  test('User can create a new transaction.', async () => {
+  it('Should be able to create a new transaction.', async () => {
     await request(app.server)
     .post('/transactions').send({
       title: 'New transaction',
@@ -19,5 +19,29 @@ describe('Transactions routes', () => {
       type: 'credit'
     })
     .expect(201)
+  })
+
+  it('Should be able to list all transactions.', async () => {
+    const createTransactionResponse = await request(app.server)
+    .post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit'
+    })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+    .get('/transactions')
+    .set('Cookie', cookies)
+    .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title:'New transaction',
+        amount: 5000,
+      })
+    ])
+    
   })
 })
